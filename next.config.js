c409c47a841e52673e22b96b8ee295ac571cbb88
BSC <puyobsc@naver.com>
2025-06-05 01:22:28 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    assetPrefix: this.assetPrefix,
     reactStrictMode: true,
     images: {unoptimized: true},
 };
